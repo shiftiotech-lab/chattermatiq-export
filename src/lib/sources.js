0177069dcecd416d356f9c09e96f7d56adf2bf65
Manjyot Rait <manjyot.aiesec@gmail.com>
2026-08-18 19:09:28 +0000

    LinkedIn: use postIds input (verified live, 3 comments); X runs but returns 0 (login-walled)

--- src/lib/sources.js
+++ src/lib/sources.js
@@ -99,7 +99,7 @@ const APIFY_ACTORS = {
   instagram: { actor: 'apify/instagram-comment-scraper', input: (url) => ({ directUrls: [url] }) },
   facebook: { actor: 'apify/facebook-comments-scraper', input: (url) => ({ startUrls: [{ url }], includeNestedComments: true, viewOption: 'RANKED_UNFILTERED' }) },
   x: { actor: 'apidojo/twitter-replies-scraper', input: (url) => ({ urls: [url] }) },
-  linkedin: { actor: 'apimaestro/linkedin-post-comments-replies-engagements-scraper-no-cookies', input: (url) => ({ url }) },
+  linkedin: { actor: 'apimaestro/linkedin-post-comments-replies-engagements-scraper-no-cookies', input: (url) => ({ postIds: [url] }) },
   reddit: { actor: 'newbs/reddit-comment-scraper', input: (url) => ({ postUrls: [url] }) },
 };
 
